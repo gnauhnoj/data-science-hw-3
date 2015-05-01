dab99158f1501fe3a1e329261a544bff0c3a7999
Rohit Jain <rohitjain.nsit@gmail.com>
2015-05-01 14:59:33 -0400

    fixed indent

--- ml_recommender.py
+++ ml_recommender.py
@@ -4,5 +4,5 @@ from parse_movies.py import get_train_data
 if __name__ == '__main__':
 	train_data = get_train_data()
 	cf = collaborative_filtering(train_data)
-    cf.train()
+	cf.train()
     # get test data
